5856dfbe9c402484a949185c0b7287db70fec1ff
sathishAB <37295788+sathishAB@users.noreply.github.com>
2018-05-24 12:39:48 +0530

    Update rm int.py

--- rm int.py
+++ rm int.py
@@ -1,6 +1,6 @@
 class hi:
     def roman_to_int(self, s):
-        rom_val = {'x': 10, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        rom_val = {'x'=10,'I'=1,'M'=1000}
         int_val = 0
         for i in range(len(s)):
             if (i > 0) and rom_val[s[i]] > rom_val[s[i - 1]]:
@@ -8,6 +8,6 @@ class hi:
             else:
                 int_val += rom_val[s[i]]
         return (int_val)
-print(hi().roman_to_int('MMMVI'))
-print(hi().roman_to_int('MMMM'))
-print(hi().roman_to_int('C'))
+print(hi().roman_to_int('XI'))
+print(hi().roman_to_int('XX'))
+print(hi().roman_to_int('M'))
